504d445e9e956f89ab35c957eb556ba946085abc
Jake Screen <jakescreen@wifi-roaming-128-4-130-146.host.udel.edu>
2020-02-27 17:35:12 -0500

    twitch proto is returning data

--- src/components/Twitch.js
+++ src/components/Twitch.js
@@ -2,6 +2,27 @@ import React from "react";
 
 export default class Twitch extends React.Component {
 
+    UNSAFE_componentWillMount(){
+        let url = "https://api.twitch.tv/helix/users/follows?from_id=455825055"
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Client-ID': 'rc8uqc4k9iv82b8l339oymibbd3nkb'
+            }
+        }).then(Response => Response.json())
+        .then(function(data){
+            let strURL = "https://api.twitch.tv/helix/streams?user_login=monstercat";
+            return fetch(strURL, {
+                method: 'GET',
+                headers: {
+                    'Client-ID': 'rc8uqc4k9iv82b8l339oymibbd3nkb'
+                }
+            })
+        })
+        .then(Response => (Response.json()))
+        .then(data1 => console.log(data1))
+    }
+
     render() {
         return(
             <div className="twitchMain">
