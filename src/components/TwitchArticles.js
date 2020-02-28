faceff7b9a721ac3382ab3fb2288b4916ddaa494
Jake Screen <jakescreen@wifi-roaming-128-4-130-146.host.udel.edu>
2020-02-27 21:37:14 -0500

    onClick ready when embed is here

--- src/components/TwitchArticles.js
+++ src/components/TwitchArticles.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 export default class TwitchArticles extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick (e, id) {
+        console.log(id);
+    }
+    
     render() {
         var yeet = [];
         //pushes props into array on relaod and displays, emptying on rerender
         yeet.push(this.props.usrs.map((item, i) => {
-            return <div className="post" key={i}>
+            return <div className="post" key={i} onClick={(e) => this.handleClick(e,item.name)}>
                 <h4>{item.name}</h4>
                 <p>{item.title}</p>
                 </div>
