b2d948786b7cca13832f7f0b1fdaf5148485e83f
Jake Screen <jakescreen@wifi-roaming-128-4-130-146.host.udel.edu>
2020-02-29 16:18:36 -0500

    looking good

---
+++ build/precache-manifest.2bdfd778939330e51402324d35622c70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7887e3f92e5e3351b3815f3a424f6739",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76ec77d8534eb1d2e9c1",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cf97f61e4328106d6a91",
+    "url": "/static/css/main.4092c72b.chunk.css"
+  },
+  {
+    "revision": "76ec77d8534eb1d2e9c1",
+    "url": "/static/js/2.c5c88f44.chunk.js"
+  },
+  {
+    "revision": "bfcb049a51b4adcbc68fdfb002dc51e4",
+    "url": "/static/js/2.c5c88f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf97f61e4328106d6a91",
+    "url": "/static/js/main.b265211f.chunk.js"
+  },
+  {
+    "revision": "8815cd9142daebee5e05",
+    "url": "/static/js/runtime-main.54478205.js"
+  }
+]);
